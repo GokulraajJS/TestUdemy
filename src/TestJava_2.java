0a5c00493b9e77f2d762c6f13762b0ac5a7aec97
ubuntu <ubuntu@ubuntu-virtual-machine>
2024-06-04 11:38:24 +0530

    This is a new commit from test_branch

---
+++ src/TestJava_2.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		string hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("Eat Something.....");
+		else {
+			System.out.println("Do work.....");
+		}
+	}
+}
+ 
