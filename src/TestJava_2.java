4485d70445621f9435c17da8fbae7d5a663c65c9
ubuntu <ubuntu@ubuntu-virtual-machine>
2024-06-04 11:56:07 +0530

    Added Corrections

--- src/TestJava_2.java
+++ src/TestJava_2.java
@@ -5,7 +5,7 @@ public class Test {
 		
 		string hungry = "yes";
 		
-		if (hungry == "yes")
+		if (hungry == "yes" || hungry == "Y")
 			System.out.println("Eat Something.....");
 		else {
 			System.out.println("Do work.....");
